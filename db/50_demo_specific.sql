983076ba4e4e0fc01cffd023ec85f8b484136375
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-02-22 22:33:05 +0100

    new db

--- db/50_demo_specific.sql
+++ db/50_demo_specific.sql
@@ -13,7 +13,7 @@ from start_version_update('1', 'Initial version', _component := 'demo_specific')
 
 
 create
-    or replace function load_permission_initial_data()
+    or replace function load_initial_demo_data()
     returns setof int
     language plpgsql
 as
@@ -22,18 +22,18 @@ declare
     __group_id int;
 begin
 
-    perform unsecure.create_perm_set_as_system('Primary tenant member', null, true, _is_assignable := true,
-                                               _permissions := array ['system.manage_tenants.get_groups'
+    perform unsecure.create_perm_set_as_system('Primary tenant member', false, true,
+                                                array ['system.manage_tenants.get_groups'
                                                    , 'system.manage_tenants.get_users']);
-    perform unsecure.create_user_group('initial_script', 1, 'Default primary tenant members', 1, true, true, false, false, true);
-    perform unsecure.assign_permission_as_system(1,4,null,'primary_tenant_member');
+    perform unsecure.create_user_group('initial_script', 1, 'Default primary tenant members');
+    perform unsecure.assign_permission_as_system(1,null,'primary_tenant_member');
 end
 
 
 $$;
 
 select *
-from load_permission_initial_data();
+from load_initial_demo_data();
 
 select *
 from stop_version_update('1', 'demo_specific');
--- db/99_fix_permissions.sql
+++ db/99_fix_permissions.sql
@@ -10,3 +10,4 @@
 
 grant usage on schema
     const, unsecure, error, ext, auth, helpers to keen_auth_permissions_demo;
+
